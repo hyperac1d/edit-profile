c1a5d4d39bc0d0e42244d14b491992ac468f98c5
Marco Mat <Marco Mat>
2017-07-17 20:09:53 +0800

    third commit

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,15 @@
- $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
 
- $(document).ready(function() {
+$('#textarea1').trigger('autoresize');
+
+$(document).ready(function() {
+    Materialize.updateTextFields();
+});
+
+$(document).ready(function() {
     $('select').material_select();
+});
+
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 50 // Creates a dropdown of 15 years to control year
   });
